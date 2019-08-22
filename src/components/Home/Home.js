5d8ba3df52be8026f10068ae476b8456666a45a4
bazmehdi <basim96@hotmail.co.uk>
2019-08-22 15:22:56 +0100

    Added Title and Timestamp outside of image for later mobile configuration

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,11 +19,14 @@ class Home extends Component {
     return (
         <div className="wrapper">
         {this.state.shot.map(data =>
+          <div>
             <div key={data.id} id={data.image_id} className="box" style={{backgroundImage: 'url(../../../images/' + data.shot_path + ')'}}>
               <div className="overlay"></div>
-              <h3>{data.title} | {data.timestamp}</h3>
+              <h3 className="desktop">{data.title} | {data.timestamp}</h3>
             </div>
-          )}
+            <h3 className="mobile">{data.title} | {data.timestamp}</h3>
+          </div>
+        )}
         </div>
     );
   }
